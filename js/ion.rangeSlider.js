5bdf4e25ea10131d85cad787fad7c7459685ac96
David Davis <david.davis@dealer.com>
2016-02-18 14:27:09 -0500

    UMD Definition so we can load easily everywhere

--- js/ion.rangeSlider.js
+++ js/ion.rangeSlider.js
@@ -1,4 +1,4 @@
-﻿// Ion.RangeSlider
+﻿// Ion.RangeSlider (UMD Version)
 // version 2.1.2 Build: 350
 // © Denis Ineshin, 2015
 // https://github.com/IonDen
@@ -10,7 +10,34 @@
 // http://ionden.com/a/plugins/licence-en.html
 // =====================================================================================================================
 
-;(function ($, document, window, navigator, undefined) {
+// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     "use strict";
 
     // =================================================================================================================
@@ -2303,5 +2330,5 @@
                 clearTimeout(id);
             };
     }());
+}));
 
-} (jQuery, document, window, navigator));
